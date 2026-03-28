d92a3e38eb2d0d72fa39c5bad66dc95fdeb0d2cf
wizardforcel <562826179@qq.com>
2026-03-28 17:16:46 +0800

    2026-03-28 17:16:46

--- update_all.ps1
+++
@@ -1,6 +0,0 @@
-git add -A
-git commit -am "$(date "+%Y-%m-%d %H:%M:%S")"
-git push git@github.com:opendoccn/freelearn-dl-zh
-git push git@gitee.com:opendoccn/freelearn-dl-zh
-git push git@gitcode.com:opendoccn1/freelearn-dl-zh
-git push git@gitcode.net:opendoccn/freelearn-dl-zh
